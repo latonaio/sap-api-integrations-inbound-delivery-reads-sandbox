c13e79282c56d026a1932ca8563ed62b4d8c25e2
MiyuYokoyama0109 <miyu.y@latona.io>
2022-04-29 11:09:30 +0900

    release

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	sap_api_caller "sap-api-integrations-inbound-delivery-reads/SAP_API_Caller"
-	"sap-api-integrations-inbound-delivery-reads/sap_api_input_reader"
+	"sap-api-integrations-inbound-delivery-reads/SAP_API_Input_Reader"
 
-	"github.com/latonaio/golang-logging-library/logger"
+	"github.com/latonaio/golang-logging-library-for-sap/logger"
 )
 
 func main() {
 	l := logger.NewLogger()
 	fr := sap_api_input_reader.NewFileReader()
-	inoutSDC := fr.ReadSDC("./Inputs/SDC_Inbound_Delivery_sample.json")
+	inoutSDC := fr.ReadSDC("./Inputs/SDC_Inbound_Delivery_Header_sample.json")
 	caller := sap_api_caller.NewSAPAPICaller(
 		"https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/", l,
 	)
@@ -18,12 +18,13 @@ func main() {
 	accepter := inoutSDC.Accepter
 	if len(accepter) == 0 || accepter[0] == "All" {
 		accepter = []string{
-			"Header", 
+			"Header", "Item",
 		}
 	}
 
 	caller.AsyncGetInboundDelivery(
 		inoutSDC.InboundDelivery.DeliveryDocument,
+		inoutSDC.InboundDelivery.DeliveryDocumentItem.DeliveryDocumentItem,
 		accepter,
 	)
 }
